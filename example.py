eea30eb976462b142edb8c5490dc05494d782a0a
lmyyao <mydreambei@gmail.com>
2019-09-29 18:00:52 +0800

    Update example.py

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ from .alert import LackAlert
 from .monitor import Monitor
 
 if __name__ == "__main__":
-    LackAlert.set_hook_id("e067ec6bd0414db7b4fc62fbcf585e87")
+    LackAlert.set_hook_id("xxxxxx")
     monitor = Monitor()
     monitor.add_handle(CPUMonitHandle("cpu", 15, 10, AlertHandleClass=LackAlert, debug=True))
     monitor.add_handle(DiskMonitHandle("disk", 20, 25, AlertHandleClass=LackAlert, debug=True))
